8f1e36d64d3c4176244d379370c2b48d237416dd
Pranav Patil <pranavrgpatil@gmail.com>
2026-08-06 11:59:01 +0530

    Connect with backend

--- portfolio_backend/portfolio/views.py
+++ portfolio_backend/portfolio/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        Contact.objects.create(
+            name=request.POST.get("name"),
+            email=request.POST.get("email"),
+            phone=request.POST.get("phone"),
+            message=request.POST.get("message"),
+        )
+        return redirect("home")
+
+    return render(request, "index.html")
